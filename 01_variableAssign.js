b849ad2a5b63c222d208ce484da37d073a4ee941
ashwinitavadar93 <ashwinitavadar93@gmail.com>
2023-12-26 13:44:55 +0530

    variable declare initialize
    variable declare initialize

---
+++ 01_variableAssign.js
@@ -0,0 +1,27 @@
+var myFullName;
+console.log(myFullName);
+
+myFullName="Ashwini Tavadar";
+console.log(myFullName)
+
+myFullName="Ashwini Vijay Tavadar";
+console.log("My Full Name Is:",myFullName);
+
+var myLove="I love only JS";
+console.log(myLove);
+
+var firstName="Ashwini";
+var lastname="Tavadar";
+var age="30";
+var address="Pune Hinjewadi";
+var pinCode="411065";
+var gender="Female";
+var isMarried="Yes";
+
+console.log("First Name:",firstName);
+console.log("Last Name:",lastname);
+console.log("Age:",age);
+console.log("Address:",address);
+console.log("Pincode:",pinCode);
+console.log("Gender:",gender);
+console.log("Married:",isMarried);
---
+++ helloWord.js
@@ -0,0 +1,25 @@
+console.log("Hello JS , I am Ashwini");
+
+ 
+
+var myName; // Varibale Declartion
+
+console.log(myName); // undefined
+
+ 
+
+myName = "Ashwini"; // Variable Intialization
+
+console.log("First Name: ",myName);
+
+ 
+
+var age = "30"; // Varibale declaration and Initialization
+
+console.log("Age:", age);
+
+ 
+
+myName = "Ashwini Tavadar"; // Varibale update
+
+console.log("Full name: ", myName);
