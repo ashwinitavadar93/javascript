e0ceaa8a9ed85c0427c43a06f9353c23bf8b0219
ashwinitavadar93 <ashwinitavadar93@gmail.com>
2024-01-02 15:19:37 +0530

    even odd
    even odd

---
+++ 05_operatorB.js
@@ -0,0 +1,106 @@
+
+console.log("======================================Step 1 ============================");
+
+function greaterNumber(){
+   var a=10;
+
+    var b=-10;
+     var gre =(a>b) ? a : b;
+     console.log(`Given No are a=10 ;b=-10`);
+    console.log(`The greatest number is =>${gre}`);
+
+    console.log("====================================== ============================");
+
+     var n1=800;
+     var n2=899;
+     var grea = (n1>n2)?n1:n2;
+     console.log(`Given No are n1=800 ;n2=899`);
+     console.log(`The greatest number is => ${grea}`);
+}
+
+greaterNumber()
+
+console.log(("=====================================Step 2======================="));
+
+function isEvenOrOddNum(){
+
+    var num=29;
+    var result=num%2;
+    var resultValue = result== 0 ? " Given number is Even " : "It is Odd number"; 
+    console.log("Given no is:29");
+    console.log(resultValue);
+
+    console.log("=============================================================");
+
+    var num=44;
+    var result=num%2;
+    var resultValue=result== 0 ? " Given number is Even " : "It is Odd number"; 
+    console.log("Given no is:44");
+    console.log(resultValue);
+
+    console.log("=============================================================");
+
+    var num=0;
+    var result=num%2;
+    var resultValue=result== 0 ? " Given number is Even " : "It is Odd number"; 
+    console.log("Given no is:0");
+    console.log(resultValue);
+
+    console.log("=============================================================");
+
+
+    var num=101;
+    var result=num%2;
+    var resultValue=result== 0 ? " Given number is Even " : "It is Odd number"; 
+    console.log("Given no is:101");
+    console.log(resultValue);
+
+}
+isEvenOrOddNum()
+
+
+    console.log("==================================step 3===========================");
+    function wordlength(){
+
+        var str="JavaScript";
+        var total=str.length;
+        console.log(`lenght of string ${total}`);
+        var num=10;
+     var str=total%2;
+     var resultValue=str== 0 ? " Given number is Even " : "It is Odd number"; 
+    console.log(resultValue);
+
+    console.log("=============================================================");
+
+
+
+    var str="Developer";
+    var total=str.length;
+    console.log(`lenght of string ${total}`);
+    var num=10;
+ var str=total%2;
+ var resultValue=str== 0 ? " Given number is Even " : "It is Odd number"; 
+console.log(resultValue);
+
+console.log("=============================================================");
+
+
+
+var str="Google";
+var total=str.length;
+console.log(`lenght of string ${total}`);
+var num=10;
+var str=total%2;
+var resultValue=str== 0 ? " Given number is Even " : "It is Odd number"; 
+console.log(resultValue);
+
+
+
+    }
+    wordlength()
+
+    
+
+
+
+
